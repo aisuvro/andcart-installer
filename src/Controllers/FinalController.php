44bb84bedb5bb44d0c8c093609acba690d94ac5b
Md. Al Imran Suvro <2842856+aisuvro@users.noreply.github.com>
2025-11-03 18:15:36 +0600

    Update namespace for dependencies in FinalController

--- src/Controllers/FinalController.php
+++ src/Controllers/FinalController.php
@@ -13,9 +13,9 @@ class FinalController extends Controller
     /**
      * Update installed file and display finished view.
      *
-     * @param  \Jmrashed\LaravelInstaller\Helpers\InstalledFileManager  $fileManager
-     * @param  \Jmrashed\LaravelInstaller\Helpers\FinalInstallManager  $finalInstall
-     * @param  \Jmrashed\LaravelInstaller\Helpers\EnvironmentManager  $environment
+     * @param  \Aisuvro\AndcartInstaller\Helpers\InstalledFileManager  $fileManager
+     * @param  \Aisuvro\AndcartInstaller\Helpers\FinalInstallManager  $finalInstall
+     * @param  \Aisuvro\AndcartInstaller\Helpers\EnvironmentManager  $environment
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function finish(InstalledFileManager $fileManager, FinalInstallManager $finalInstall, EnvironmentManager $environment)
